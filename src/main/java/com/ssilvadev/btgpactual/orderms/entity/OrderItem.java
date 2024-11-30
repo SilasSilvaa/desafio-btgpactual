a1589f8bdf720194f5a950b84f0e35f1b976b008
SilasSilvaa <silas-oli@hotmail.com>
2024-11-30 16:17:14 -0300

    feat: add order entity

---
+++ src/main/java/com/ssilvadev/btgpactual/orderms/entity/OrderItem.java
@@ -0,0 +1,22 @@
+package com.ssilvadev.btgpactual.orderms.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+
+import java.math.BigDecimal;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+
+    private String product;
+    private Integer quantity;
+
+    @Field(targetType = FieldType.DECIMAL128)
+    private BigDecimal price;
+}
